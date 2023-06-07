2f8fd171542a5e3a691c1191a7be5dc7f444cc07
Asteriuz <gutocebola@gmail.com>
2023-06-07 18:54:48 -0300

    Added nav.js

---
+++ js/nav.js
@@ -0,0 +1,25 @@
+const toggleBtn = document.querySelector('.toggle-btn');
+const toggleBtnIcon = document.querySelector('.toggle-btn i');
+const dropDownMenu = document.querySelector('.dropdown-menu');
+
+let isOpen = false;
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle('open');
+  isOpen = dropDownMenu.classList.contains('open');
+
+  toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa solid fa-bars';
+};
+
+let prevScrollpos = window.scrollY;
+
+window.onscroll = function () {
+  let currentScrollPos = window.scrollY;
+  console.log(currentScrollPos);
+  if (prevScrollpos > currentScrollPos || currentScrollPos < 64 || isOpen) {
+    document.getElementById("navbar").style.transform = "translateY(0)";
+  } else {
+    document.getElementById("navbar").style.transform = "translateY(-100%)";
+  }
+  prevScrollpos = currentScrollPos;
+};
